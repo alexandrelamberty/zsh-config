d06dcda0b779e87b838d731517c839ec94ad2bc7
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-08-24 01:06:06 +0200

    feat: add git session function

--- config/.config/zsh/functions.zsh
+++ config/.config/zsh/functions.zsh
@@ -1,28 +1,67 @@
 #!/bin/sh
 
-# Git --------------
+# Tmux create git session ession based on the current git repository and its submodules.
+# https://stackoverflow.com/a/49001088/9241975
+function tmgs() {
+    isGitDirectory() {
+        if [ -d ".git" ]
+        then
+            # # Use -d to allow the rest of the function to run
+            project=${PWD##*/}
+            # Check if session already exist
+            tmux new-session -d -s $project
+            tmux new-window -n $project
+            # Tmux create a window named after the shell used
+            tmux kill-window -t zsh
+            # Open README.md with nvim if available
+            # TODO: EDITOR env variable
+            # TODO: check for README.md
+            tmux send-keys -t $project:$project 'nvim README.md' Enter
+            
+            # Check for submodules
+            # -d to prevent current window from changing
+            git submodule foreach 'tmux new-window -d -n $name'
+            
+            # -d to detach any other client
+            tmux attach-session -d -t $project
+            
+            return 0
+        else
+            return 1
+        fi
+    }
+    
+    if isGitDirectory; then ; else echo "Not a Git repository!"; fi
+    
+}
 
+# Tmux attach session
 function tmas() {
     local fsession
     fsession=$(tmux ls | sed -E "s/:.*$//" | fzf --reverse --height 40%) || return
     tmux attach-session -t "$fsession"
 }
 
+# Tmux kill session
 function tmks() {
     local fsession
     fsession=$(tmux ls | sed -E "s/:.*$//" | fzf --reverse --height 40%) || return
     tmux kill-session -t "$fsession"
 }
 
+# GIT
+
+# Git clone repository
 gc () {
     git clone git@github.com:${1}
 }
 
+# Git clone personal repository
 gcr () {
     git clone git@github.com:alexandrelamberty/${1}
 }
 
-# Function to source files if they exist
+# Source files if they exist
 function zsh_add_file() {
     [ -f "$ZDOTDIR/$1" ] && source "$ZDOTDIR/$1"
 }
