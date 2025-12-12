2d5243b8192803858070db11170cc6f4147fa935
Alexandre Lamberty <alexandrelamberty@gmail.com>
2025-12-12 19:31:21 +0100

    feat: add ZSH paths configuration file for user and language binaries

---
+++ config/.config/zsh/paths.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+# ~/.config/zsh/paths.zsh
+# ZSH Paths Configuration File
+
+# User Local Bin 
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/share/bin/:$PATH"
+
+# Specific Language Binaries
+export PATH="$GOBIN:$PATH"
